4a9e467eaba2181084a80edd11afe741a2bffe8a
JacobMason83 <jmason062083@gmail.com>
2021-06-07 20:13:30 -0400

    added animations to main landing page for site

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,14 @@
 function Home() {
     return (
-        <div>
-            Hi from home 
+        <div className='HomeContainer'>
+        <video src="../styles/videos/smoke.mp4" type='video/mp4' ></video>
+            <div className="HomeWrapper">
+                <div className="HomeClickHeader">
+                {/* need to add a click to the headline */}
+                    <h1>Welcome to the Witch Pattern Company</h1>
+                    <p>We sell everything from bags, t-shirts,coffee mugs, altar cloths, and many more click to come inside and see the marvels that lie within!!!</p>
+                </div>
+            </div>
         </div>
     )
 }
